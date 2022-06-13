d7a7b75b758269d07d16e8a7f9b8df3a7b959a70
Joel Tumambo <joeltumambs@gmail.com>
2022-06-13 20:12:06 +0800

    Fix createParkingSpots

--- src/utils/createParkingSpots.tsx
+++ src/utils/createParkingSpots.tsx
@@ -1,20 +1,20 @@
+import cuid from "cuid";
 import { flow } from "lodash";
 import ParkingSpot, { Size } from "../types/ParkingSpot";
 
-const createSize: (entries: number, spot: number) => Size = flow(
-  (entries, spot) => [Math.floor(spot / entries), entries],
-  ([index, entries]) => Math.min(index, entries - 1),
-  (index) => Object.values(Size)[index]
+const createSize: (index: number, total: number) => Size = flow(
+  (index, total) => Math.floor(index / (total / 3)),
+  (sizeKey) => Object.values(Size)[sizeKey]
 );
 
 const createDistances = (entries: number, spot: number): number[] =>
   [...Array(entries)].map((_, entry) => spot + entry + 1);
 
 const createParkingSpots = (entries: number, spots: number): ParkingSpot[] =>
-  [...Array(spots)].map((_, spot) => ({
-    id: spot,
-    size: createSize(entries, spot),
-    distances: createDistances(entries, spot),
+  [...Array(spots)].map((_, index) => ({
+    id: cuid.slug(),
+    size: createSize(index, spots),
+    distances: createDistances(entries, index),
   }));
 
 export default createParkingSpots;
